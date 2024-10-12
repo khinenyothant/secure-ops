2e4f1a948c56b2703c45f4b89fb8a1fd9dcb4da5
KhineNyoThant <khinenyothant@gmail.com>
2024-10-12 23:50:18 +0200

    update env

--- 7.enable-aws-auth-on-vault/create-workspace-env/variables.tf
+++ 7.enable-aws-auth-on-vault/create-workspace-env/variables.tf
@@ -1,50 +1,50 @@
 variable "workspace_name" {
   description = "Workspace Name"
-  type = string
-  default = "7_enable-aws-auth-on-vault"
+  type        = string
+  default     = "7_enable-aws-auth-on-vault"
 }
 variable "org_name" {
   description = "Organization Name"
-  type = string
-  default = "hellocloud-knt"
+  type        = string
+  default     = "hellocloud-knt"
 }
 variable "vault_url" {
   description = "The address of the Vault instance runs will access."
-  type = string
-  default = "https://vault-cluster-id-public-vault-2a5d42b2.18e736ba.z1.hashicorp.cloud:8200" ### have to change new vault cluster id
+  type        = string
+  default     = "https://vault-cluster-public-vault-d956a58b.a68d84be.z1.hashicorp.cloud:8200" ### have to change new vault cluster id
 }
 variable "run_role" {
   description = "TFC_VAULT_RUN_ROLE"
-  type = string
-  default = "admin-role"
+  type        = string
+  default     = "admin-role"
 }
 variable "vault_namespace" {
   description = "TFC_VAULT_NAMESPACE"
-  type = string
-  default = "admin"
+  type        = string
+  default     = "admin"
 }
 variable "aws_region" {
   description = "AWS_REGION"
-  type = string
-  default = "ap-northeast-1"
+  type        = string
+  default     = "ap-northeast-1"
 }
 variable "tfc_vault_backed_aws_auth" {
   description = "TFC_VAULT_BACKED_AWS_AUTH"
-  type = string
-  default = "true"
+  type        = string
+  default     = "true"
 }
 variable "tfc_vault_backed_aws_auth_type" {
   description = "TFC_VAULT_BACKED_AWS_AUTH_TYPE "
-  type = string
-  default = "iam_user"
+  type        = string
+  default     = "iam_user"
 }
 variable "tfc_vault_backed_aws_run_vault_role" {
   description = "TFC_VAULT_BACKED_AWS_RUN_VAULT_ROLE"
-  type = string
-  default = "admin-access-role"
+  type        = string
+  default     = "admin-access-role"
 }
 variable "tfc_vault_backed_aws_mount_path" {
   description = "TFC_VAULT_BACKED_AWS_MOUNT_PATH"
-  type = string
-  default = "aws-master-vault-admin"
+  type        = string
+  default     = "aws-master-vault-admin"
 }
