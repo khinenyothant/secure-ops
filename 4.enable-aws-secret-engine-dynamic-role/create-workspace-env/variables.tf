1f42cbfc56e7910566ced9d475e12b5ef0cb9931
KhineNyoThant <khinenyothant@gmail.com>
2024-10-12 21:53:05 +0200

    4.enable-aws-secret-engine-dynamic-role

--- 4.enable-aws-secret-engine-dynamic-role/create-workspace-env/variables.tf
+++ 4.enable-aws-secret-engine-dynamic-role/create-workspace-env/variables.tf
@@ -1,30 +1,30 @@
 variable "workspace_name" {
   description = "Workspace Name"
-  type = string
-  default = "enable-aws-secret-engine-dynamic-role"
+  type        = string
+  default     = "4_enable-aws-secret-engine-dynamic-role"
 }
 variable "org_name" {
   description = "Organization Name"
-  type = string
-  default = "hellocloud-knt"
+  type        = string
+  default     = "hellocloud-knt"
 }
 variable "vault_url" {
   description = "The address of the Vault instance runs will access."
-  type = string
-  default = "https://vault-cluster-id-public-vault-2a5d42b2.18e736ba.z1.hashicorp.cloud:8200" ### have to change new vault cluster id
+  type        = string
+  default     = "https://vault-cluster-public-vault-d956a58b.a68d84be.z1.hashicorp.cloud:8200" ### have to change new vault cluster id
 }
 variable "run_role" {
   description = "TFC_VAULT_RUN_ROLE"
-  type = string
-  default = "admin-role"
+  type        = string
+  default     = "admin-role"
 }
 variable "vault_namespace" {
   description = "TFC_VAULT_NAMESPACE"
-  type = string
-  default = "admin"
+  type        = string
+  default     = "admin"
 }
 variable "aws_region" {
   description = "AWS_REGION"
-  type = string
-  default = "ap-northeast-1"
+  type        = string
+  default     = "ap-northeast-1"
 }
